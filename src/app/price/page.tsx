bff7f94fc5319f9e4a5cac29862417402e038438
omkaradstia <omkar@adstia.com>
2024-12-12 15:32:26 +0530

    Fix build issue

--- src/app/price/page.tsx
+++ src/app/price/page.tsx
@@ -1,32 +1,40 @@
 "use client";
 
+import React, { Suspense } from "react";
 import Footer from "@/components/static/Footer";
 import Header from "@/components/static/Header";
 import Nav from "@/components/static/Nav";
 import PriceCard from "@/components/static/PriceCard";
 import { useSearchParams } from "next/navigation";
 
-export default function PricePage() {
+const PricePageContent: React.FC = () => {
     const searchParams = useSearchParams();
     const category = searchParams.get("category");
 
     return (
+        <main>
+            {category ? (
+                <section className="price-card-section">
+                    <PriceCard category={category === "All Vehicle" ? "all" : category} />
+                </section>
+            ) : (
+                <div>
+                    <p>Please select a category to see pricing details.</p>
+                </div>
+            )}
+        </main>
+    );
+};
+
+export default function PricePage() {
+    return (
         <div className="font-geist-sans">
             <Nav />
             <Header />
-
-            <main>
-                {category ? (
-                    <section className="price-card-section">
-                        <PriceCard category={category === "All Vehicle" ? "all" : category} />
-                    </section>
-                ) : (
-                    <div>
-                        <p>Please select a category to see pricing details.</p>
-                    </div>
-                )}
-                <Footer />
-            </main>
+            <Suspense fallback={<p>Loading...</p>}>
+                <PricePageContent />
+            </Suspense>
+            <Footer />
         </div>
     );
 }
